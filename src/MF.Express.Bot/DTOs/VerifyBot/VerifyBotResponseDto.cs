1c789ca00b5f80bd765383fa18c85237b892f9bf
i.suchkov <i.suchkov@multifactor.ru>
2025-12-22 15:26:45 +0500

    fix

--- src/MF.Express.Bot/DTOs/VerifyBot/VerifyBotResponseDto.cs
+++ src/MF.Express.Bot/DTOs/VerifyBot/VerifyBotResponseDto.cs
@@ -1,11 +1,8 @@
-using System.Text.Json.Serialization;
-
 namespace MF.Express.Bot.Api.DTOs.VerifyBot;
 
 public record VerifyBotResponseDto(
-    [property: JsonPropertyName("status")] string Status,
-    [property: JsonPropertyName("bot_id")] string BotId,
-    [property: JsonPropertyName("verified")] bool Verified
+    string BotId,
+    bool Verified
 );
 
 
