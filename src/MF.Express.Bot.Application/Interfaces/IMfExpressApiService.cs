533f8bc0cd3d3afba7d3f99a0be5e1a9a1b44f61
i.suchkov <i.suchkov@multifactor.ru>
2026-02-18 10:53:54 +0500

    wip

--- src/MF.Express.Bot.Application/Interfaces/IMfExpressApiService.cs
+++ src/MF.Express.Bot.Application/Interfaces/IMfExpressApiService.cs
@@ -1,3 +1,4 @@
+using MF.Express.Bot.Application.Models.Common;
 using MF.Express.Bot.Application.UseCases.BotCommands;
 
 namespace MF.Express.Bot.Application.Interfaces;
@@ -8,10 +9,15 @@ public interface IMfExpressApiService
         string callbackData,
         string chatId,
         CancellationToken cancellationToken = default);
-    
+
     Task<bool> SendChatCreatedCallbackAsync(
         BotCommandRequest botRequest,
         string authRequestId,
+        CancellationToken cancellationToken = default);
+
+    Task<InlineEnrollResponse> SendInlineEnrollCodeAsync(
+        BotCommandRequest botRequest,
+        string code,
         CancellationToken cancellationToken = default);
 }
 
